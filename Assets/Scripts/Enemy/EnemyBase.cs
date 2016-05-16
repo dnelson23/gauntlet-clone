3d781031981d1b67e1cf593a654145ea564be6d8
dnelson23 <drwnlsn@gail.com>
2016-05-16 13:26:50 -0700

    Finished enemy base class. Added level scene

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -3,34 +3,66 @@ using System.Collections.Generic;
 
 namespace Assets.Scripts.Enemy
 {
-    public abstract class EnemyBase : Components.Generic.ControllerBase
-    {
-        Components.Generic.HitPoints _health;
-        protected float maxHP = 100f;
-        public float CurrentHP
+	public abstract class EnemyBase : Components.Generic.ControllerBase
+	{
+		Components.Generic.HitPoints _health;
+		protected float maxHP = 100f;
+		public float CurrentHP
+		{
+			get { return _health.curHitPoints; }
+			private set { _health.SetMaxHitPoints(value); }
+		}
+
+		Components.Generic.VectorMovement2D _movement;
+
+		protected float damage = 100f;
+
+		public bool isVisible = false;
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			_movement = _parent.gameObject.AddComponent<Components.Generic.VectorMovement2D>();
+
+			_health = _parent.gameObject.AddComponent<Components.Generic.HitPoints>();
+			CurrentHP = maxHP;
+		}
+
+		void Update() { }
+
+		public override void Destroy() { }
+
+
+
+        void OnCollisionStay(Collision other)
         {
-            get { return _health.curHitPoints; }
-            private set { _health.SetMaxHitPoints(value); }
+            Debug.Log("Collision");
+            Components.Generic.HitPoints playerHP = other.gameObject.GetComponent<Components.Generic.HitPoints> ();
+            if (playerHP)
+            {
+                playerHP.TakeDamage (damage);
+            }
         }
 
-        Components.Generic.VectorMovement2D _movement;
 
-        protected float damage = 100f;
 
-        public bool isVisible = false;
-
-        protected override void Awake()
-        {
-            base.Awake();
-
-            _movement = _parent.gameObject.AddComponent<Components.Generic.VectorMovement2D>();
-
-            _health = _parent.gameObject.AddComponent<Components.Generic.HitPoints>();
-            CurrentHP = maxHP;
-        }
-        
-        void Update() { }
-
-        public override void Destroy() { }
+		//Find closest Player
+		public GameObject FindClosestAlivePlayer()
+		{
+			float distance = Mathf.Infinity;
+			Hero.HeroBase[] heroRefs = FindObjectsOfType<Hero.HeroBase> ();
+			GameObject newTarget = null;
+			foreach (Hero.HeroBase player in heroRefs) {
+				if(Vector3.Distance (this.transform.position, player.gameObject.transform.position) < distance)
+				{
+					newTarget = player.gameObject;
+					distance = Vector3.Distance(this.transform.position, newTarget.transform.position);
+				}
+			}
+			if (newTarget == null)
+				Debug.Log ("No Players are Alive");
+			return newTarget;
+		}
     }
 }
